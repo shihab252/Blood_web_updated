700ed75e6b39f4cffc30f215b13e9f7f76f2dba1
Shihab uddin khan <122334148+shihab252@users.noreply.github.com>
2026-07-22 18:57:11 +0600

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -16,7 +16,7 @@ export default function Login() {
     e.preventDefault();
     setIsLoading(true);
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("https://blood-web-backend.onrender.com/api/auth/login", {
         email,
         password,
       });
